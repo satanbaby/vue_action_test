53acd60c626e266cf297a076145346afb4e28ea9
satanbaby <joe8605273512882@gmail.com>
2020-04-05 01:37:36 +0800

    使用TDD撰寫反轉字串，再至 Hello Wrold.vue 撰寫 class style get methods 反轉字串

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
+    <h1>{{ handleMessage }}</h1>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
@@ -33,10 +33,14 @@
 
 <script lang="ts">
 import { Component, Prop, Vue } from 'vue-property-decorator';
+import 'vue-class-component/hooks';
 
 @Component
 export default class HelloWorld extends Vue {
   @Prop() private msg!: string;
+  get handleMessage(): string {
+    return this.msg.split('').reverse().join('');
+  }
 }
 </script>
 
--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -2,12 +2,22 @@ import { expect } from 'chai';
 import { shallowMount } from '@vue/test-utils';
 import HelloWorld from '@/components/HelloWorld.vue';
 
+// describe('HelloWorld.vue', () => {
+//   it('renders props.msg when passed', () => {
+//     const msg = 'new message';
+//     const wrapper = shallowMount(HelloWorld, {
+//       propsData: { msg },
+//     });
+//     expect(wrapper.text()).to.include(msg);
+//   });
+// });
+
 describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message';
+  it('Hello world is reverse string中文', () => {
+    const msg = 'new messages';
     const wrapper = shallowMount(HelloWorld, {
       propsData: { msg },
     });
-    expect(wrapper.text()).to.include(msg);
+    expect(wrapper.text()).to.include(msg.split('').reverse().join(''));
   });
 });
